c6e5ccea50bd32476505bdbd768a0c85ce95386f
PZC YYJ <pzcyyj@outlook.com>
2023-11-17 17:27:35 +0800

    对象优化02

---
+++ 对象优化02/对象优化02.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+class Test
+{
+public:
+    Test(int data = 10) : ma(data) { cout << "Test(" << ma << ")" << endl; }
+    ~Test() { cout << "~Test(" << ma << ")" << endl; }
+    Test(const Test& t) : ma(t.ma) { cout << "Test(const Test& t)" << endl; }
+    void operator = (const Test& t) 
+    {
+        cout << "operator =" << endl;
+        ma = t.ma;
+    }
+    int getData() const { return ma; }
+private:
+    int ma;
+};
+
+Test GetObject(Test &t) // 不能返回局部的或者临时的对象的指针或引用
+{
+    cout << "-----------" << endl;
+    int val = t.getData();
+    cout << "------------" << endl;
+    return Test(val); 
+    // vs2017上会有一步拷贝构造产生临时对象，因为tmp函数运行结束就会析构，
+    // 只能通过临时对象传出去，但是vs22并未调用拷贝构造
+}
+
+int main()
+{
+    Test t1;
+    cout << "-------------" << endl;
+    Test t2 = GetObject(t1);
+
+    return 0;
+}
