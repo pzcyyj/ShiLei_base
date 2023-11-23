639e920414a5c2d2c8f691c5cfc91b9a881a21b7
PZC YYJ <pzcyyj@outlook.com>
2023-11-23 13:46:04 +0800

    多线程

---
+++ 多线程访问共享对象的线程安全问题/多线程访问共享对象的线程安全问题.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <memory>
+#include <thread>
+using namespace std;
+
+class A
+{
+public:
+	A() { cout << "A()" << endl; }
+	~A() { cout << "~A()" << endl; }
+	void testA() { cout << "很好的方法" << endl; }
+};
+
+void handler01(shared_ptr<A> pw)
+{
+	//std::this_thread::sleep_for(std::chrono::seconds(20));
+	// q访问a对象时，需要侦测一下A对象是否存活
+	/*shared_ptr<A> sp = pw.lock();
+	if (sp != nullptr)
+	{
+		sp->testA();
+	}
+	else
+		cout << "A对象已经析构，不能再访问" << endl;*/
+	if (pw != nullptr)
+	{
+		pw->testA();
+	}
+	else
+		cout << "A对象已经析构，不能再访问" << endl;
+}
+
+int main()
+{
+	{
+		shared_ptr<A> p(new A());
+
+		thread t1(handler01, p);
+
+		t1.detach();
+
+		p->testA();
+
+		std::this_thread::sleep_for(std::chrono::seconds(20));
+	}
+
+	
+
+	return 0;
+}
