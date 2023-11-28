e8e173f3218904d5b096e8b296b078090ce56150
PZC YYJ <pzcyyj@outlook.com>
2023-11-28 13:22:34 +0800

    单例模式

---
+++ 单例模式/单例模式.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+
+// 饿汉式单例  一定是线程安全的
+class Singleton
+{
+public:
+    static Singleton* getInstance() // #3 获取类的唯一实例对象的接口方法
+    {
+        if (instance == nullptr)
+        {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+private:
+    static Singleton* instance; // #2 定义一个唯一的类的实例对象
+    Singleton() // #1 构造函数私有化
+    {
+        std::cout << "Singleton()" << std::endl;
+    }
+    Singleton(const Singleton&) = delete;
+    Singleton& operator=(const Singleton&) = delete;
+};
+
+Singleton* Singleton::instance = nullptr;
+
+int main()
+{
+    Singleton* p1 = Singleton::getInstance();
+    Singleton* p2 = Singleton::getInstance();
+    Singleton* p3 = Singleton::getInstance();
+    std::cout << p1 << std::endl;
+    std::cout << p2 << std::endl;
+    std::cout << p3 << std::endl;
+
+    return 0;
+}
